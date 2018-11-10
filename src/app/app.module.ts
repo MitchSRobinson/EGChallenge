98d777d97c0c079590c2eace2b4f142876947cf2
Mitch Robinson <Mitch.Robinson97@gmail.com>
2018-11-10 16:14:44 +0000

    Add test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MockComponent } from './components/mock/mock.component';
+import { DataService } from './services/api-util.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MockComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    DataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/mock/mock.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/api-util.service';
+
+@Component({
+  selector: 'app-mock',
+  templateUrl: './mock.component.html',
+  styleUrls: ['./mock.component.css']
+})
+export class MockComponent implements OnInit {
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.dataService.getInstrument().subscribe(data => {
+      console.log(data);
+    });
+  }
+}
