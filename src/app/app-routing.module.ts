29fc1926fd1607a2d977bc9d042fb18695f02eae
Mitch Robinson <Mitch.Robinson97@gmail.com>
2018-11-10 19:56:12 +0000

    Add instrument to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+import { InstrumentComponent } from './components/instrument/instrument.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent}
+  { path: '', component: HomeComponent},
+  { path: 'instrument/:id', component: InstrumentComponent}
 ];
 
 @NgModule({
